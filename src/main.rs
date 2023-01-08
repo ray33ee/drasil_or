d5e6f57d0945213f38de314f66a88ad7cfec6f37
ray33ee <30669752+ray33ee@users.noreply.github.com>
2023-01-08 22:28:22 +0000

    We now calculate the hash of the shared secret, send it back via created cell from OR, and OP verifies.
    Signed-off-by: ray33ee <30669752+ray33ee@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,25 @@ use std::net::SocketAddr;
 use serde::{Serialize, Deserialize};
 
 use futures::prelude::*;
+use sha2::{Sha256, Digest};
 use tokio::net::TcpListener;
 use tokio_serde::formats::*;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 use x25519_dalek::{EphemeralSecret, PublicKey};
 
-
 #[derive(Serialize, Deserialize, Debug)]
 enum RelayType {
     Extend{public_x: [u8; 32], ip: SocketAddr},
-    Extended{public_y: [u8; 32]},
+    Extended{public_y: [u8; 32], hash: [u8; 32]},
+    Begin{ addr: String },
+    Connected,
+    Data,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 enum CellType {
     Create{public_x: [u8; 32]},
-    Created{public_y: [u8; 32]},
+    Created{public_y: [u8; 32], hash: [u8; 32]},
     Relay{recognised: u32, stream_id: u32, digest: u32, data: RelayType, padding: Vec<u8>},
 }
 
@@ -28,6 +31,7 @@ struct Cell {
     hop_id: u32,
     data: CellType,
 }
+
 
 #[tokio::main]
 pub async fn main() {
@@ -58,14 +62,23 @@ pub async fn main() {
                     let server_secret = EphemeralSecret::new(rand::rngs::OsRng);
                     let server_public = PublicKey::from(&server_secret);
 
-                    //Send the created ack
-                    framed_socket
-                        .send(Cell { hop_id: 0, data: CellType::Created {public_y: server_public.to_bytes()} })
-                        .await
-                        .unwrap();
-
                     // Calculate the shared secret
                     let onion_secret = server_secret.diffie_hellman(&PublicKey::from(public_x)).to_bytes();
+
+                    //Calculate thee hash of the shared secret
+                    let mut hasher = Sha256::new();
+
+                    hasher.update(&onion_secret);
+
+                    let ga = hasher.finalize();
+
+                    let calculated_hash = ga.as_slice();
+
+                    //Send the created ack
+                    framed_socket
+                        .send(Cell { hop_id: 0, data: CellType::Created {public_y: server_public.to_bytes(), hash: calculated_hash.try_into().unwrap()} })
+                        .await
+                        .unwrap();
 
                     println!("Shared: {:?}", onion_secret);
 
